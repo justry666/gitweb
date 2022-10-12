d63be594afddb9df5f47ecd27128270cc6db7af2
justry666 <113984196+justry666@users.noreply.github.com>
2022-10-13 00:13:27 +0800

    Add files via upload

---
+++ loginServlet.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet(name="loginServlet",urlPatterns="/loginServlet")
+    public class loginServlet extends HttpServlet{
+    @Override
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+    throws ServletException, IOException {
+        log("1");
+        response.setContentType("text/html;charset=utf-8");//设置输出编码
+        String username=request.getParameter("username");//获取用户名密码
+        String password=request.getParameter("password");
+        if(("admin").equals(username)&&("123").equals(password))//假定数据
+            response.sendRedirect("/web1/welcome.html");//判断后重定向
+        else response.sendRedirect("/web1/login.html");
+    }
+    @Override
+    public void doPost(HttpServletRequest request,HttpServletResponse response)
+    throws ServletException,IOException{
+        doGet(request,response);
+    }
+}
